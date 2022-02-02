07812838ab00d2c684b99bb578d4e70ea708abba
LujiAnna <ikokianna@gmail.com>
2022-02-02 15:54:23 +0100

    debugged client and server

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
+// Listen to event on form when user clicks SendQuote
 function sendQuote() {
   let formElement = document.getElementById("contact-form");
 
+// Get values from the form
   let nameElement = document.getElementById("name");
   let nameValue = nameElement.value;
   console.log('name: ', nameValue);
@@ -11,20 +13,34 @@ function sendQuote() {
 
   let gebouwElement = document.getElementById("gebouw");
   // error reading value using value property
- 
 
   let messageElement = document.getElementById("message");
   let messageValue = messageElement.value;
   console.log('message: ', messageValue);
-  // var text = "";
-  // var i;
-  // for (i = 0; i < formValue.length ;i++) {
-  //   text += formValue.elements[i].value + "<br>";
-  // }
-  // console.log(text);
 
-  console.log('function works');
-  // console.log(formValue.value, nameValue.value, emailValue.value, gebouwValue.value, messageValue.value);
+  // Store input values in an object
+  const mail = { nameValue, emailValue, messageValue };
 
-  // document.getElementById("demo").innerHTML = text;
+  console.log('function works: type mail below ');
+  console.log(mail);
+
+
+  // send data from form to server using base url:https://gloriousclean.be/contact.html 
+  // const url = 'https://gloriousclean.be/contact.html/send';
+const url = 'http://localhost:3000/contact.html/send'
+  // const otherPara = {
+  //   headers: {
+  //     "content-type":"application/json"; charset=UTF-8
+  //   },
+  //   body: data,
+  //   method: "POST"
+  // };
+
+  fetch(url, { // returns a promise
+    method: "post", // specify method as post
+    body: mail, // specify body as mail to send to request
+  }) 
+  .then(response => response.json()) // converting promise to JSON
+  .then(res => console.log(res)) // view the response from server later after server is set
+  .catch(error=>console.log(error))
 }
--- server.js
+++ server.js
@@ -1,29 +1,37 @@
 const express = require('express');
+const nodemailer = require('nodemailer');
+const multiparty = require('multiparty');
+require('dotenv').config();
+
+// instantiate the express app
 const app = express();
 const port = 3000;
 const log = console.log;
 const path = require('path');
 
-// Parse data for processing
-app.use(express.urlencoded)({
-  extended: false})
-});
+// For POST and PUT request
+// parse the body from post/fetch request
+// app.use(express.json());
+// Parse html post forl for processing
+// app.use(express.urlencoded)({ extended: false});
 
-app.use(express.json());
-
-// send the html file into this slot
-app.get('/contact', (req, res) => {
-  // res.send('Hello World!');
-  res.sendFile(path.join(__dirname, 'contact.html'))
-});
+// app.use(express.static('public'));
 
 // send email
-app.post('/email', (req, res) => {
-  // send msg back to client
-  res.json({message: 'Message received !!'})
-});
+// app.post('/email', (req, res) => {
+//   // access the incoming data and display it on terminal
+//   console.log('Data: ', req.body);
+//   // send msg back to client
+//   res.json({message: 'Message received !!'})
+// });
 
+// send the html file into this slot
+// app.get('/contact', (req, res) => {
+//   // res.send('Hello World!');
+//    // Before res.send() 
+//     console.log(res.headersSent); 
+//     // res.send('OK');
+//   res.sendFile(path.join(__dirname, 'contact.html'))
+// });
 
-});
-
-app.listen(port, () => log(`Server app listening at http://localhost:${port}/contact`));
+// app.listen(port, () => log(`Server app listening at http://localhost:${port}`));
